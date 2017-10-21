c4a590728e559d350c82956f18ee80c613dd2c72
Dirk Chang <dirk@kooiot.com>
2017-10-21 18:33:21 +0800

    Version action buttons

--- app_center/app_center/doctype/iot_application_version/iot_application_version.js
+++ app_center/app_center/doctype/iot_application_version/iot_application_version.js
@@ -3,6 +3,35 @@
 
 frappe.ui.form.on('IOT Application Version', {
 	refresh: function(frm) {
-
+		if (frm.doc.tested == 0 || !frm.doc.tested_date) {
+			frm.add_custom_button(__("Tested"), function () {
+				frm.events.set_tested(frm);
+			}).removeClass("btn-default").addClass("btn-primary");
+		}
+		if (frm.doc.approved == 0 || !frm.doc.approved_date) {
+			frm.add_custom_button(__("Approve"), function () {
+				frm.events.set_approved(frm);
+			}).removeClass("btn-default").addClass("btn-primary");
+		}
+	},
+	set_tested: function(frm) {
+		return frappe.call({
+			doc: frm.doc,
+			method: "set_tested",
+			freeze: true,
+			callback: function(r) {
+				if(!r.exc) frm.refresh_fields();
+			}
+		})
+	},
+	set_approved: function(frm) {
+		return frappe.call({
+			doc: frm.doc,
+			method: "set_approved",
+			freeze: true,
+			callback: function(r) {
+				if(!r.exc) frm.refresh_fields();
+			}
+		})
 	}
 });
--- app_center/app_center/doctype/iot_application_version/iot_application_version.py
+++ app_center/app_center/doctype/iot_application_version/iot_application_version.py
@@ -4,6 +4,7 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe.utils import now
 from frappe.model.document import Document
 
 class IOTApplicationVersion(Document):
@@ -19,3 +20,13 @@ class IOTApplicationVersion(Document):
 			app_remove(self.app, self.version)
 		except Exception as ex:
 			frappe.logger(__name__).error(ex.message)
+
+	def set_tested(self):
+		self.set("tested", 1)
+		self.set("tested_date", now())
+		self.save()
+
+	def set_approved(self):
+		self.set("approved", 1)
+		self.set("approved_date", now())
+		self.save()

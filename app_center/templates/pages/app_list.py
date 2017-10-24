bc8215597c2d20c582aee00355d42cd81b199564
Dirk Chang <dirk@kooiot.com>
2017-10-24 20:55:08 +0800

    application center layout template

---
+++ app_center/templates/pages/app_list.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017, Dirk Chang and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import throw, _
+
+
+def get_context(context):
+	context.no_cache = 1
+	context.show_sidebar = False
+
+	context.doc = {
+	}

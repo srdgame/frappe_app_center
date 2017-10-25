70b757a51027d2c72c589dd76e2e7c34624e5fa3
Dirk Chang <dirk@kooiot.com>
2017-10-25 12:49:33 +0800

    template layout

--- app_center/templates/pages/app_list.py
+++ app_center/templates/pages/app_list.py
@@ -8,9 +8,6 @@ from frappe import throw, _
 
 
 def get_context(context):
-	if frappe.session.user == 'Guest':
-		frappe.local.flags.redirect_location = "/login"
-		raise frappe.Redirect
 
 	context.no_cache = 1
 	context.show_sidebar = True

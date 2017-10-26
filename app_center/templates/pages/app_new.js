1a545a720c2d98e6603611aa6853fe6be516409f
Dirk Chang <dirk@kooiot.com>
2017-10-26 16:39:49 +0800

    application pages

--- app_center/templates/pages/app_new.js
+++ app_center/templates/pages/app_new.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
-	$.ajaxSetup({
-		headers: { // 默认添加请求头
-			"X-Frappe-CSRF-Token": frappe.csrf_token
-		}
-	});
 	var status = $('.ui.form .ui.message');
 	$('form').ajaxForm({
 		beforeSend: function() {

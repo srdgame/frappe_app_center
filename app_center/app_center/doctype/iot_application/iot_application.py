3cc814b0e6877a34b892c15b5545abaabef94989
Dirk Chang <dirk@kooiot.com>
2017-10-27 14:02:22 +0800

    description in markdown editor

--- app_center/app_center/doctype/iot_application/iot_application.py
+++ app_center/app_center/doctype/iot_application/iot_application.py
@@ -30,3 +30,13 @@ class IOTApplication(Document):
 					throw(_("Application path is not an valid path!"))
 		self.app_ext = self.app_ext.lower()
 		self.app_name_unique = self.owner + "/" + self.app_name
+
+	def before_insert(self):
+		applist = frappe.db.get_values('IOT Application', {"owner": self.owner})
+		group = frappe.get_value("App Developer", self.owner, "group")
+		if len(applist) > 10 and group == 'Normal':
+			throw(_("Application count limitation!"))
+		if len(applist) > 20 and group == 'Power':
+			throw(_("Application count limitation!"))
+		if len(applist) > 100 and group == 'Admin':
+			throw(_("Application count limitation!"))
---
+++ app_center/appmgr.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017, Dirk Chang and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+import os
+import requests
+from frappe import throw, msgprint, _
+from werkzeug.utils import secure_filename
+
+
+
+ALLOWED_EXTENSIONS = set(['csv', 'CSV', 'zip', 'ZIP', 'gz', 'GZ'])
+
+
+def allowed_file(filename):
+	# 用于判断文件后缀
+	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+def decode_input(form_dict, key):
+	val = form_dict.get(key)
+	if val:
+		return val.decode('utf-8')
+
+
+def decode_input_checkbox(form_dict, key):
+	val = form_dict.get(key)
+	if val:
+		return val.decode('utf-8') == 'on'
+	return False
+
+
+def app_remove(app, version):
+	basedir = frappe.db.get_single_value('App Center Settings', 'release_folder')
+	file_dir = os.path.join(basedir, app)
+
+	ext = frappe.get_value("IOT Application", app, "app_ext")
+
+	filename = str(version) + '.' + ext  # 修改了上传的文件名
+	os.remove(os.path.join(file_dir, filename))
+
+
+@frappe.whitelist()
+def app_upload():
+	if frappe.request.method != "POST":
+		throw(_("Request Method Must be POST!"))
+
+	version = int(decode_input(frappe.form_dict, 'version'))
+	app = decode_input(frappe.form_dict, 'app')
+	app_name = decode_input(frappe.form_dict, 'app_name')
+	owner = decode_input(frappe.form_dict, 'owner') or frappe.session.user
+	beta = decode_input_checkbox(frappe.form_dict, 'beta')
+
+	if not version:
+		throw(_("Application version not found!"))
+
+	if not app or not app_name:
+		throw(_("Application name not found!"))
+
+	if not frappe.request.files:
+		throw(_("Application file not found!"))
+
+	if frappe.get_value("IOT Application Version", {"app": app, "version":version}, "name"):
+		throw(_("Application version duplicated!"))
+
+	f = frappe.request.files['app_file']  # 从表单的file字段获取文件，app_file为该表单的name值
+	fname = secure_filename(f.filename)
+
+	if f and allowed_file(fname):  # 判断是否是允许上传的文件类型
+		basedir = frappe.db.get_single_value('App Center Settings', 'release_folder')
+		# file_dir = os.path.join(basedir, owner)
+		# if not os.path.exists(file_dir):
+		# 	os.makedirs(file_dir)
+		file_dir = os.path.join(basedir, app)
+		if not os.path.exists(file_dir):
+			os.makedirs(file_dir)
+
+		ext = fname.rsplit('.', 1)[1].lower()  # 获取文件后缀
+		if ext != frappe.get_value("IOT Application", app, "app_ext"):
+			throw(_("Appication file extension name incorrect!"))
+
+		new_filename = str(version) + '.' + ext  # 修改了上传的文件名
+		# if beta:
+		# 	new_filename = "beta_" + new_filename
+		# print(new_filename)
+		f.save(os.path.join(file_dir, new_filename))  # 保存文件到upload目录
+
+		data = {
+			"doctype": "IOT Application Version",
+			"app": app,
+			"app_name": app_name,
+			"version": version,
+			"owner": owner,
+			"beta": 0,
+		}
+		if beta:
+			data.update({"beta": 1})
+
+		try:
+			doc = frappe.get_doc(data).insert()
+			doc.save()
+		except Exception as ex:
+			os.remove(os.path.join(file_dir, new_filename))
+			throw(_("Application version creation failed!"))
+
+		return _("Application upload success")
+	else:
+		throw(_("Application upload failed!"))
+
+
+@frappe.whitelist()
+def app_new():
+	if frappe.request.method != "POST":
+		throw(_("Request Method Must be POST!"))
+
+	app_name = decode_input(frappe.form_dict, 'app_name')
+	license_type = decode_input(frappe.form_dict, 'license_type')
+	category = decode_input(frappe.form_dict, 'category')
+	protocol = decode_input(frappe.form_dict, 'protocol')
+	device_supplier = decode_input(frappe.form_dict, 'device_supplier')
+	device_serial = decode_input(frappe.form_dict, 'device_serial')
+	description = decode_input(frappe.form_dict, 'description')
+	owner = frappe.session.user
+	doc = frappe.get_doc({
+		"doctype": "IOT Application",
+		"app_name": app_name,
+		"license_type": license_type,
+		"category": category,
+		"protocol": protocol,
+		"device_supplier": device_supplier,
+		"device_serial": device_serial,
+		"owner": owner,
+		"description": description,
+	}).insert()
+	return doc
+
+
+@frappe.whitelist()
+def app_modify():
+	if frappe.request.method != "POST":
+		throw(_("Request Method Must be POST!"))
+
+	app_name = decode_input(frappe.form_dict, 'app_name')
+	category = decode_input(frappe.form_dict, 'category')
+	protocol = decode_input(frappe.form_dict, 'protocol')
+	device_supplier = decode_input(frappe.form_dict, 'device_supplier')
+	device_serial = decode_input(frappe.form_dict, 'device_serial')
+	description = decode_input(frappe.form_dict, 'description')
+
+	app = decode_input(frappe.form_dict, 'app')
+	doc = frappe.get_doc("IOT Application", app)
+	doc.set("app_name", app_name)
+	doc.set("category", category)
+	doc.set("protocol", protocol)
+	doc.set("device_supplier", device_supplier)
+	doc.set("device_serial", device_serial)
+	doc.set("description", description)
+	doc.save()
+
+
+@frappe.whitelist()
+def app_fork():
+	if frappe.request.method != "POST":
+		throw(_("Request Method Must be POST!"))
+
+	version = int(decode_input(frappe.form_dict, 'version'))
+	app = decode_input(frappe.form_dict, 'app')
+	if not frappe.get_value('IOT Application Version', {"app": app, "version": version}, "name"):
+		throw(_("Application version does not exists!"))
+
+	doc = frappe.get_doc("IOT Application", app)
+	owner = frappe.session.user
+
+	new_doc = frappe.get_doc({
+		"doctype": "IOT Application",
+		"app_name": doc.app_name + "." + version,
+		"license_type": doc.license_type,
+		"category": doc.category,
+		"protocol": doc.protocol,
+		"device_supplier": doc.device_supplier,
+		"device_serial": doc.device_serial,
+		"owner": owner,
+		"description": doc.description,
+	}).insert()
+	return new_doc

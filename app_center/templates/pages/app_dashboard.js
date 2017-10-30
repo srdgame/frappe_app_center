b358482bb35e070326af291297ff30f472ab31b8
Dirk Chang <dirk@kooiot.com>
2017-10-30 16:17:33 +0800

    Application center dashboard initial page

---
+++ app_center/templates/pages/app_dashboard.js
@@ -0,0 +1,35 @@
+function jsonToQueryString(json) {
+    return '?' +
+        Object.keys(json).map(function(key) {
+            return encodeURIComponent(key) + '=' +
+                encodeURIComponent(json[key]);
+        }).join('&');
+};
+
+$(document).ready(function() {
+	$('.ui.app-upload.button').click(function () {
+		var app = $(this).attr("app");
+		window.location.href="/app_upload?app="+app;
+	});
+	$('.ui.app-modify.button').click(function () {
+		var app = $(this).attr("app");
+		window.location.href="/app_modify?app="+app;
+	});
+	$('.ui.app-add.button').click(function () {
+		window.location.href="/app_new";
+	});
+	$('.ui.app-refresh.button').click(function () {
+		var category = $('#category_filter .menu .item.selected').attr("value");
+		if (category != null) {
+			window.location.href="/app_list"+jsonToQueryString({"category":category});
+		} else {
+			window.location.href = "/app_list";
+		}
+	});
+	$('.ui.app-clean.button').click(function () {
+		//window.location.href="/app_list";
+		$('#category_filter .text').text('{{_("Category Filter")}}');
+		$('#category_filter .menu .item.selected').removeClass("active selected");
+	});
+});
+
---
+++ app_center/templates/pages/app_dashboard.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017, Dirk Chang and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import throw, _
+
+
+def get_context(context):
+	if frappe.session.user == 'Guest':
+		frappe.local.flags.redirect_location = "/login"
+		raise frappe.Redirect
+
+	context.no_cache = 1
+
+	category = frappe.form_dict.category
+
+	context.categories = [d.name for d in frappe.db.get_all("App Category", ["name"], order_by="name")]
+
+	filters = {"owner": frappe.session.user}
+	if category:
+		filters["category"] = category
+
+	context.apps = frappe.db.get_all("IOT Application", "*", filters, order_by="modified desc")
+
+	context.filters = filters

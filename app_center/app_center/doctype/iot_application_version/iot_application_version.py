2ecb1ade86bbf118b9e2541866a1ae55b77b03ca
Dirk Chang <dirk@kooiot.com>
2017-10-31 19:25:07 +0800

    Check max version

--- app_center/app_center/doctype/iot_application_version/iot_application_version.py
+++ app_center/app_center/doctype/iot_application_version/iot_application_version.py
@@ -4,12 +4,18 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import throw, _
 from frappe.utils import now
 from frappe.model.document import Document
+
 
 class IOTApplicationVersion(Document):
 	def validate(self):
 		self.app_name = frappe.get_value('IOT Application', self.app, 'app_name')
+		if self.is_new():
+			max_version = frappe.db.sql("select max(version) from `tabIOT Application Version` where app=`{0}`".format(self.app))
+			if max_version > self.version:
+				throw(_("Version must be bigger than {0}").format(max_version))
 
 	def autoname(self):
 		self.name = self.app + "." + str(self.version)

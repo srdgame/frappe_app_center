04c569e11d8ac908adcf3228582978da6b7a233a
Dirk Chang <dirk@kooiot.com>
2017-10-31 17:57:10 +0800

    preview the icon change

--- app_center/templates/pages/app_new.js
+++ app_center/templates/pages/app_new.js
@@ -1,2 +1,8 @@
 $(document).ready(function() {
+	$("#icon_file").change(function(event){
+		var name=$(this).attr("name");
+		var val = $(this).val();
+		var tmppath = URL.createObjectURL(event.target.files[0]);
+		$('.ui.image.button .image').attr('src', tmppath);
+	});
 });

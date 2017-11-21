c0bd00568e39e32ffbe4eb41c8778854298884cb
Dirk Chang <dirk@kooiot.com>
2017-11-21 18:08:50 +0800

    dashboard improve

--- app_center/templates/pages/app_dashboard.js
+++ app_center/templates/pages/app_dashboard.js
@@ -1,30 +1,16 @@
 $(document).ready(function() {
-	$('.ui.segments .ui.basic.segment.no-padding-bottom .right.floated.header .ion-ios-arrow-up.icon.link').click(function(){
+	$('.ui.segments .ui.segment .right.floated.header .ion-ios-arrow-up.icon.link').click(function(){
 		if ($(this).hasClass('ion-ios-arrow-up')) {
-			$('.ui.segments .ui.basic.segment.no-padding .flot-chart').hide();
+			$(this.parentNode.parentNode.parentNode).find('.ui.segment.content').hide();
 			$(this).removeClass('ion-ios-arrow-up').addClass('ion-ios-arrow-down');
 		} else {
-			$('.ui.segments .ui.basic.segment.no-padding .flot-chart').show();
+			$(this.parentNode.parentNode.parentNode).find('.ui.segment.content').show();
 			$(this).removeClass('ion-ios-arrow-down').addClass('ion-ios-arrow-up');
 		}
 	});
-	$('.ui.segments .ui.basic.segment.no-padding-bottom .right.floated.header .ion-ios-close-empty.icon.link').click(function() {
+	$('.ui.segments .ui.segment .right.floated.header .ion-ios-close-empty.icon.link').click(function() {
 		$(this.parentNode.parentNode.parentNode).hide();
 	});
 
-	$('.ui.clearing.segment .right.floated.header .ion-ios-arrow-up.icon.link').click(function(){
-		if ($(this).hasClass('ion-ios-arrow-up')) {
-			$(this.parentNode.parentNode).find('.ui.grid').hide();
-			$(this).removeClass('ion-ios-arrow-up').addClass('ion-ios-arrow-down');
-		} else {
-			$(this.parentNode.parentNode).find('.ui.grid').show();
-			$(this).removeClass('ion-ios-arrow-down').addClass('ion-ios-arrow-up');
-		}
-
-	});
-
-	$('.ui.clearing.segment .right.floated.header .ion-ios-close-empty.icon.link').click(function(){
-		$(this.parentNode.parentNode).hide();
-	});
 });
 

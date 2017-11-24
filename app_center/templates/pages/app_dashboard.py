a34168ee537da852dc01013e884c897b79871437
Dirk Chang <dirk@kooiot.com>
2017-11-24 10:51:31 +0800

    Web page improve

--- app_center/templates/pages/app_dashboard.py
+++ app_center/templates/pages/app_dashboard.py
@@ -35,3 +35,13 @@ def get_context(context):
 	context.most_stars = get_most_stars()
 	context.recently_updated = get_recently_updated()
 	context.releases = get_latest_releases()
+
+
+	count_apps = "select count(*) from `tabIOT Application` where owner<>'Administrator'" # and published=1
+	context.application_count = frappe.db.sql(count_apps)[0][0]
+
+	count_developers = "select count(*) from `tabApp Developer` where user<>'Administrator'"
+	context.developer_count = frappe.db.sql(count_developers)[0][0]
+
+	count_suppliers = "select count(*) from `tabApp Device Supplier`"
+	context.supplier_count = frappe.db.sql(count_suppliers)[0][0]

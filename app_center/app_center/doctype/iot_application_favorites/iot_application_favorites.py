cdfa4bd27624b64bd1fa7fa251bb73d35327b812
Dirk Chang <dirk@kooiot.com>
2019-11-12 11:05:49 +0800

    Fixed store.favorites feature

--- app_center/app_center/doctype/iot_application_favorites/iot_application_favorites.py
+++ app_center/app_center/doctype/iot_application_favorites/iot_application_favorites.py
@@ -7,4 +7,29 @@ from __future__ import unicode_literals
 from frappe.model.document import Document
 
 class IOTApplicationFavorites(Document):
-	pass
+	def append_favorites(self, *favorites):
+
+		"""Add favorite applications to list"""
+		current_favorites = [d.app for d in self.get("favorites")]
+		for fav in favorites:
+			app = fav.get('app')
+			comment = fav.get('comment')
+			priority = fav.get('priority')
+
+			if app in current_favorites:
+				continue
+
+			self.append("favorites", {"app": app, "comment": comment, "priority": priority})
+
+	def add_favorites(self, *favorites):
+		"""Add groups to favorite applications and save"""
+		self.append_favorites(*favorites)
+		self.save()
+
+	def remove_favorites(self, *favorites):
+		existing_favorites = dict((d.app, d) for d in self.get("favorites"))
+		for app in favorites:
+			if app in existing_favorites:
+				self.get("favorites").remove(existing_favorites[app])
+
+		self.save()

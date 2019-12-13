4fabd581917a97a4d712440021698fea4e28debd
Dirk Chang <dirk@kooiot.com>
2019-12-13 11:57:58 +0800

    Checking the nickname and id card number

--- app_center/app_center/doctype/app_developer_requisition/app_developer_requisition.py
+++ app_center/app_center/doctype/app_developer_requisition/app_developer_requisition.py
@@ -4,12 +4,29 @@
 
 from __future__ import unicode_literals
 import frappe
+import re
 from frappe import throw
 from frappe.model.document import Document
+from app_center.app_center.doctype.app_developer.app_developer import check_id_card
+
+match_nickname = re.compile(r'([a-z]\w+)', re.I)
 
 
 class AppDeveloperRequisition(Document):
 	def validate(self):
+		m = match_nickname.match(self.nickname)
+		if not m:
+			throw('invalid_nick_name')
+		nickname_g = m.groups()
+		if len(nickname_g) == 0:
+			throw('invalid_nick_name')
+		if nickname_g[0] != self.nickname:
+			throw('invalid_nick_name')
+
+		valid, err = check_id_card(self.id_card)
+		if not valid:
+			throw(err)
+
 		if self.docstatus == 0:
 			if frappe.db.exists("App Developer", {"user": self.user}):
 				throw("developer_exists")
@@ -37,7 +54,7 @@ class AppDeveloperRequisition(Document):
 			"doctype": "App Developer",
 			"user": self.user,
 			"group": 'Normal',
-			"nickname": self.domain,
+			"nickname": self.nickname,
 			"id_card": self.id_card,
 			"address": self.address,
 			"pay_bank": self.pay_bank,
